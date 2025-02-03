9edd4f90079d632516ff6c857130380deb6830f5
STARRED787 <malithamalshan7@gmail.com>
2025-02-03 13:31:08 +0530

    PHP

--- config/database.php
+++ config/database.php
@@ -4,22 +4,39 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-$dotenv->load();
-
 $capsule = new Capsule;
 
-$capsule->addConnection([
-    'driver'    => 'mysql',
-    'host'      => $_ENV['DB_HOST'],
-    'database'  => $_ENV['DB_NAME'],
-    'username'  => $_ENV['DB_USER'],
-    'password'  => $_ENV['DB_PASS'],
-    'charset'   => 'utf8',
-    'collation' => 'utf8_unicode_ci',
-    'prefix'    => '',
-    'port'      => '3306'
-]);
+// Check if running on Railway
+if (getenv('RAILWAY_ENVIRONMENT')) {
+    // Use Railway's provided MySQL credentials
+    $capsule->addConnection([
+        'driver'    => 'mysql',
+        'host'      => getenv('DB_HOST'),
+     
+        'database'  => getenv('DB_NAME'),
+        'username'  => getenv('DB_USER'),
+        'password'  => getenv('DB_PASS'),
+        'charset'   => 'utf8',
+        'collation' => 'utf8_unicode_ci',
+        'prefix'    => '',
+    ]);
+} else {
+    // Local development credentials from .env
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+    $dotenv->load();
+
+    $capsule->addConnection([
+        'driver'    => 'mysql',
+        'host'      => $_ENV['DB_HOST'],
+        'database'  => $_ENV['DB_NAME'],
+        'username'  => $_ENV['DB_USER'],
+        'password'  => $_ENV['DB_PASS'],
+        'charset'   => 'utf8',
+        'collation' => 'utf8_unicode_ci',
+        'prefix'    => '',
+        'port'      => '3306'
+    ]);
+}
 
 // Make this Capsule instance available globally
 $capsule->setAsGlobal();
